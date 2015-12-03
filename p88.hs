23b87320b5fe61633a3ba397c268bc19e5d8298a
Ben Sklaroff <bsklaroff@gmail.com>
2015-12-03 08:19:29 -0500

    add p88

---
+++ p88.hs
@@ -0,0 +1,22 @@
+import Data.List
+
+main = do putStrLn . show $ connectedcomponents (Graph [1,2,3,4,5,6,7] [(1,2),(2,3),(1,4),(3,4),(5,2),(5,4),(6,7)])
+
+data G a = Graph [a] [(a, a)] deriving (Show, Eq)
+
+depthfirst :: (Eq a, Ord a) => G a -> a -> [a]
+depthfirst (Graph ns es) startN = sort $ depthfirst' (Graph ns es) [] (Just startN)
+  where depthfirst' _ res Nothing = res
+        depthfirst' (Graph ns es) res (Just n)
+          | n `elem` res = res
+          | otherwise    = foldr (\otherN acc -> depthfirst' (Graph ns es) acc otherN) (n:res) (map (maybeOtherN n) es)
+        maybeOtherN n (n0, n1)
+          | n == n0   = Just n1
+          | n == n1   = Just n0
+          | otherwise = Nothing
+
+connectedcomponents :: (Eq a, Ord a) => G a -> [[a]]
+connectedcomponents (Graph gns ges) = cc (Graph gns ges) gns
+  where cc _ [] = []
+        cc g (n:ns) = nextClump : cc g (filter (`notElem` nextClump) ns)
+          where nextClump = depthfirst g n
